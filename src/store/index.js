820517b630bd6a7ffa183833ab68c1e14fe3a61a
Robert Pratt <bpong@v8ch.com>
2018-06-26 21:35:21 +0700

    Feature complete

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { apiRequest } from '@wordpress/api-request';
 import { dispatch, registerStore } from '@wordpress/data';
 
-const DEFAULT_STATE = { categories: null };
+const DEFAULT_STATE = {
+	categories: null,
+	postsInPrimaryCategory: null,
+};
 
-registerStore( 'primary-category', {
+registerStore( 'v8ch/primary-category', {
 	reducer( state = DEFAULT_STATE, action ) {
 		switch ( action.type ) {
 			case 'SET_CATEGORIES':
@@ -29,7 +32,7 @@ registerStore( 'primary-category', {
 		},
 	},
 	resolvers: {
-		async fetchCategories() {
+		async getCategories() {
 			const categories = await apiRequest( { path: '/wp/v2/categories?per_page=100' } );
 			dispatch( 'v8ch/primary-category' ).setCategories( categories );
 		},
